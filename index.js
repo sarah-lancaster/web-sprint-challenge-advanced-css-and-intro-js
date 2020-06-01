13fc0a6dfd357db222dac582f92957be785bee4b
TylerOliverTaylor <61921198+TylerOliverTaylor@users.noreply.github.com>
2020-05-31 22:32:40 -0400

    task 3 complete

--- index.js
+++ index.js
@@ -231,11 +231,11 @@ console.log (artists[8].name)
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(id, name) {
-    /* code here */
-  }
+function getArtistByIndex(artists, index) {
+  console.log (`The artist at index ${artists[index].id} is ${artists[index].name}.`);
+}
   
-  /**
+
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born the 20th century (1900-2000) */
